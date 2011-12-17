5217463ce86ac0a6fd8db2bbc5af7ed573ed03ec
Samuel Levy <sam@samuellevy.com>
2011-12-17 03:38:23 +0000

    Added the API to get the next generation

---
+++ getnext.php
@@ -0,0 +1,48 @@
+<?php
+// pull in the required functions
+require_once('functions.php');
+
+// make a new generation!
+new_generation();
+
+// check if we want the next evolution, or the latest evolution
+if (isset($_POST['last']) && strlen(trim($_POST['last']))>=7) {
+    // split the 'last' into it's component parts = a 6 char key, and the rest is the ID
+    $k = substr(trim($_POST['last']),0,6);
+    $id = intval(substr(trim($_POST['last']),6));
+    
+    $lv = check_generation($k,$id);
+    
+    if ($lv) {
+        $nv = next_generation($id);
+        
+        if ($nv) {
+            // return the full position, and assume that it's blank
+            $gen = array('key'=>$nv['ref'],
+                         'change'=>map_to_array($nv['change']));
+        } else {
+            $gen = false;
+        }
+    } else {
+        $error = "Generation doesn't exist";
+    }
+} else {
+    // just get the latest generation
+    $nv = latest_generation();
+    
+    // return the full position, and assume that it's blank
+    $gen = array('key'=>$nv['ref'],
+                 'change'=>map_to_array($nv['position'],true));
+}
+
+// check for an error
+if (isset($error)) {
+    $data = array("success"=>false,"info"=>$error);
+} else {
+    $data = array("success"=>true,"generation"=>$gen);
+}
+
+// return the response
+header('Content-type: application/javascript');
+echo json_encode ($data);
+?>
